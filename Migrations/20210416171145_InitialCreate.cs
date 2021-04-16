2b6379a5afe55b57cf688d864233537db979dc31
korewatabit <63434430+korewatabit@users.noreply.github.com>
2021-04-16 19:19:43 +0200

    Add files via upload

---
+++ Migrations/20210416171145_InitialCreate.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace zad2.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StudentDB",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Imie = table.Column<string>(type: "TEXT", nullable: true),
+                    Nazwisko = table.Column<string>(type: "TEXT", nullable: true),
+                    Ocena = table.Column<double>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudentDB", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StudentDB");
+        }
+    }
+}
